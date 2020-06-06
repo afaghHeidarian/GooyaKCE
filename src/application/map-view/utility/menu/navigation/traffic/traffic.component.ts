bfcf6eec0e202b48474aa579b530eb68f17f063c
Afagh Heidarian <afag.heidarian@yahoo.com>
2020-06-06 11:19:24 +0430

    create project with GIT

---
+++ src/application/map-view/utility/menu/navigation/traffic/traffic.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { PublicVarService } from 'src/application/shared/services/public-var.service';
+import { MapService } from 'src/application/shared/services/map.service';
+
+@Component({
+ selector: 'app-traffic',
+ template: `
+    <label class="switch">
+      <input type="checkbox" #checkboxTraffic (change)="switchTraffic(checkboxTraffic)"
+      [checked]="this.publicVar.isTrafficON" />
+      <div class="slider"></div>
+    </label>
+  `,
+})
+export class TrafficComponent implements OnInit {
+ constructor(public publicVar: PublicVarService, private mapservice: MapService) {}
+
+ ngOnInit() {}
+
+ switchTraffic(TrafficInput: HTMLInputElement) {
+  if (TrafficInput.checked) {
+   this.publicVar.isTrafficON = true;
+   this.mapservice.map.addLayer(this.publicVar.WMTSLayerTraffic);
+   console.log('Traffic.checked');
+  } else {
+   this.publicVar.isTrafficON = false;
+   this.mapservice.map.removeLayer(this.publicVar.WMTSLayerTraffic);
+
+  }
+  this.publicVar.status.traffic = this.publicVar.isTrafficON ;
+  localStorage.setItem('Status', JSON.stringify(this.publicVar.status));
+ }
+}
